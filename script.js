5ecf7fda370860f1e0566890be2faf04f2c73778
Mohammed Kedir <imamekdr@gmail.com>
2022-01-28 19:10:46 +0000

    final (add highlite color and title)

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function compute()
         document.getElementById("principal").focus();
     } else {
         //display calculation result
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML="If you deposit <span class='hilightText'>"+principal+"</span>,\<br\>at an interest rate of <span class='hilightText'>"+rate+"%</span>\<br\>You will receive an amount of <span class='hilightText'>"+interest+"</span>,\<br\>in the year <span class='hilightText'>"+year+"</span>\<br\>"
     }
 }
 
